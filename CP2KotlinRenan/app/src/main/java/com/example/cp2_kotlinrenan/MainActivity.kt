ae5caf728f377764fd73f268e2c049c90c65e582
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-14 21:26:17 -0300

    spinner configurado

--- CP2KotlinRenan/app/src/main/java/com/example/cp2_kotlinrenan/MainActivity.kt
+++ CP2KotlinRenan/app/src/main/java/com/example/cp2_kotlinrenan/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.cp2_kotlinrenan
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,17 +23,45 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val items = listOf("CRIPTCOS", "RENDEY LLC", "PRAETOR LLC")
 
-        val edt1 = findViewById<EditText>(R.id.editTextText)
-        val botao = findViewById<Button>(R.id.button)
-        val edt2 = findViewById<TextView>(R.id.textView)
+        // Criar um ArrayAdapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
 
-        botao.setOnClickListener {
-            val
-            text = edt1.text.toString()
-            edt2.text = "Você digitou: $text"
+        // Configurar o Spinner
+        val spinner = findViewById<Spinner>(R.id.spinner1)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Você selecionou: $selectedItem", Toast.LENGTH_SHORT
+                ).show()
+
+
+                val edt1 = findViewById<EditText>(R.id.editTextText)
+                val botao = findViewById<Button>(R.id.button)
+                val edt2 = findViewById<TextView>(R.id.textView)
+
+                botao.setOnClickListener {
+                    val
+                            text = edt1.text.toString()
+                    edt2.text = "Você digitou: $text"
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+
         }
     }
-
-
 }
